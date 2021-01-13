954b20725fc6e5c684b2b652d27cf675e57800ed
cybermarinella <marinella.mastrosimone@gmail.com>
2021-01-13 21:36:45 +0100

    comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import logo from './logo.svg'; // import the logo from mockup
 import './App.css';
 
 function App() {
   return (
     <div>
 
-      <h1>Welcome Alka</h1>
+      <h1>Welcome</h1>
       <h3>Junior UI Designer Execise</h3>
       <p>
         In this exercise you will try to compile this same page by replacing this content with the one present in the <a href="https://preview.uxpin.com/46bf69245b33229db6458867238e26620cc540eb#/pages/134803925">Mockup</a>
